b7c8b21de9e20a3c83c5c929a23ef39defdb72ae
lucas-perata <contacto.perata@gmail.com>
2024-10-19 22:44:53 -0300

    fixes on loop

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,6 @@
 #include <util/delay.h>
 
 
-#define SERVO_MIN_PULSE_WIDTH 500  // 1ms pulse width (minimum position)
-#define SERVO_MAX_PULSE_WIDTH 1500  // 2ms pulse width (maximum position)
-#define SERVO_PERIOD 20000          // 20ms total period (50Hz)
-
-
 SoftwareSerial espSerial(1, 0); // RX, TX
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // Address 0x27, 16 columns, 2 rows
 
@@ -36,11 +31,10 @@ enum Category {
   Test
 };
 
-unsigned long segundos = 1500; 
-int breakSeconds = 500;
+unsigned long segundos = 5; // 1500 segundos = 25 minutos
+short int breakSeconds = 5; // 500 = 5 minutos
 bool timerIsRunning = false; 
 bool breakTimerIsRunning = false;
-bool isBoxOpen = false;
 Category selectedCategory = 0;
 byte count  = 59;
 // variable malloc para almacenar los timers terminados
@@ -59,12 +53,13 @@ init();
   // PORTD
   DDRD = 0b00011101;
 
-  // LED on PORTD2 and Buzzer on PORTD3
+  // LED on PORTD2 and LED on PORTD3 Buzzer on PORTD4
  // DDRD = (1 << DDD2) | (1 << DDD3);  
   PORTD &= ~(1 << PORTD2);           
   PORTD &= ~(1 << PORTD3);     
+  PORTD &= ~(1 << PORTD4);
 
-  // PORTB (botones)
+  // PORTB (botones) 0 al 3
   DDRB = 0b00000000; 
   PORTB = (1 << PINB0) | (1 << PINB1) | (1 << PINB2) | (1 << PINB3);  
 
@@ -77,31 +72,21 @@ init();
     TCCR1B = (1 << CS12); // Set prescaler to 64
 
 
-    setServoAngle(500);
 
   while (1) {
     
     if (ButtonPressed(0, PINB, 0, 100)) {
-      lcd.clear();
-      lcd.print("Coloca tu celu");
-      lcd.setCursor(0, 1);
-      lcd.print("en la caja");
-      if(isBoxOpen == false)
+      if(!timerIsRunning)  
       {
-        setServoAngle(1500);
-       _delay_ms(4000);
-        setServoAngle(500);
+        timerIsRunning = true;
+        PORTD ^= (1 << PORTD2);  // Toggle LED
+        PORTD ^= (1 << PORTD4);  // Toggle Buzzer
       }
-      else {
-        _delay_ms(4000);
-        setServoAngle(500);
-      }
-     
-
-      PORTD ^= (1 << PORTD2);  // Toggle LED
-      PORTD ^= (1 << PORTD3);  // Toggle Buzzer
-      if(!timerIsRunning) {timerIsRunning = true;}
-     else {timerIsRunning = false;}
+      else 
+      {
+        PORTD ^= (1 << PORTD2);
+        timerIsRunning = false;
+        }
     } 
 
 
@@ -112,54 +97,50 @@ init();
 
     if(ButtonPressed(2, PINB, 2, 100))
     {
-      isBoxOpen = true; 
-      setServoAngle(1500);
-      timerIsRunning = false;
-      PORTD ^= 1 << PORTD3;
+      PORTD &= (1 << 1);
       lcd.clear();
       lcd.print("Timer reseteado");
       resetTimer();
     }
 
     basicTimer();
-    
   }
 
 }
 
 void basicTimer()
 {
-   if(segundos > 0 && timerIsRunning) 
+   if(segundos > 0) 
    {
-    if (TCNT1 > 63000)
+    if (TCNT1 > 63000 && timerIsRunning)
       {
+          PORTD &= (1<<2); // Turn off buzzer
           TCNT1 = 0;
           segundos--;
           // TODO Fix lcd clear
           lcd.clear();
           lcd.setCursor(0, 0);
           lcd.print("Timer -> ");
-          lcd.print(segundos / 60);
+          lcd.print(segundos);
           lcd.print(":");
           lcd.print(count--);
           printCat();
           if(count == 0) {count = 59;}
       }
    }
-   else if( segundos == 0 && !timerIsRunning)
+   else if( segundos == 0 )
    {
-    timerIsRunning = false;
-    breakTimerIsRunning = true;
     breakTimer();
    }
 }
 
 void breakTimer()
 {
-  setServoAngle(1500);
-  if(breakSeconds > 0 && breakTimerIsRunning)
+  // TODO: Implementar break timer LCD
+  if(breakSeconds > 0)
   {
-    if (TCNT1 > 63000)
+    // TODO: Leds Apagar 2 y prender 3
+    if (TCNT1 > 63000 && timerIsRunning)
       {
           TCNT1 = 0;
           breakSeconds--;
@@ -169,14 +150,14 @@ void breakTimer()
   }
   else 
   {
-    breakTimerIsRunning = false;
-    setServoAngle(500);
     resetTimer();
   }
 }
 
 void resetTimer()
 {
+  breakTimerIsRunning = false; 
+  timerIsRunning = false;
   segundos = 1500;
   count = 59;
 }
@@ -186,7 +167,7 @@ void connectToWIFI() {
 
   espSerial.println("AT");
   _delay_ms(1000);
-  espSerial.println("AT+CWJAP=\"Fibertel WiFi131 2.4GHz\",\"00439434465\"");
+  espSerial.println("AT+CWJAP=\"Fibertel  2.4GHz\",\"\"");
 }
 
 void setupI2C_LCD() {
@@ -220,27 +201,7 @@ void selectCategory() {
 }
 
 
-void setServoAngle(uint16_t pulseWidth) {
-    // Ensure pulse width is within valid range (1ms to 2ms)
-    if (pulseWidth < SERVO_MIN_PULSE_WIDTH) pulseWidth = SERVO_MIN_PULSE_WIDTH;
-    if (pulseWidth > SERVO_MAX_PULSE_WIDTH) pulseWidth = SERVO_MAX_PULSE_WIDTH;
-    
-    // Generate the PWM signal manually by toggling the pin
-    PORTD |= (1 << PORTD4);  // Set PORTD4 high (start pulse)
-    custom_delay_us(pulseWidth);   // Wait for the pulse width duration
-    PORTD &= ~(1 << PORTD4); // Set PORTD4 low (end pulse)
 
-    // Wait for the rest of the 20ms period
-    custom_delay_us(SERVO_PERIOD - pulseWidth);
-}
-
-void custom_delay_us(uint16_t us) {
-    // Loop to create a delay for the given number of microseconds
-    while (us--) {
-        // Each iteration of this loop takes approximately 4 clock cycles
-        _delay_loop_1(4); 
-    }
-}
 
 void printCat() 
 {
