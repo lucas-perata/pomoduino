12f04ba215378b46889c66c7963aa5f7e0d97cce
lucas-perata <contacto.perata@gmail.com>
2024-10-23 17:04:56 -0300

    Added potentiometer + pomo count + pomo selection

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,14 @@ void connectToWIFI();
 void printESPResponse();
 void setupI2C_LCD(); 
 void resetTimer();
+void continueTimer();
 void selectCategory();
 byte convertSeconds();
 void setServoAngle(uint16_t pulseWidth);
 void custom_delay_us(uint16_t us);
 void printCat();
 void clearRow(byte row);
+void selectPomodoro(uint16_t pV);
 
 
 enum Category {
@@ -33,14 +35,18 @@ enum Category {
   Test
 };
 
+#define ADC_CHANNEL 7 // A7 corresponds to ADC channel 7
+
+
 unsigned long segundos = 5; // 1500 segundos = 25 minutos
 short int breakSeconds = 5; // 500 = 5 minutos
+int selectSeconds = 5; // Valor por default
+int selectBreakSeconds = 5; // Valor por default
 bool timerIsRunning = false; 
 bool breakTimerIsRunning = false;
 Category selectedCategory = 0;
 byte count  = 59;
-// variable malloc para almacenar los timers terminados
-// o si no también meter un postrequest siempre que termine un timer
+byte pomos = 0; 
 
 // TODO: track timers completados en una variable
 
@@ -51,12 +57,24 @@ void printESPResponse() {
   }
 }
 
+void ADC_Init() {
+    ADMUX = (1 << REFS0) | (ADC_CHANNEL); // AVCC with external capacitor at AREF pin, select ADC channel
+    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Enable ADC, prescaler = 128
+}
+
+uint16_t ADC_Read(uint8_t channel) {
+    ADMUX = (ADMUX & 0xF0) | (channel & 0x0F); // Select ADC channel
+    ADCSRA |= (1 << ADSC); // Start conversion
+    while (ADCSRA & (1 << ADSC)); // Wait for conversion to complete
+    return ADCW; // Read ADC value
+}
+
 int main(void) {
   sei();
   init();
-  Wire.begin();     
+  Wire.begin();   
+  ADC_Init();  
   
-  setupI2C_LCD();
 
   // PORTD
   DDRD = 0b10011101;
@@ -79,26 +97,30 @@ int main(void) {
     TCCR1A = 0; // Set Timer1 to Normal mode (WGM13:0 = 0)
     TCCR1B = (1 << CS12); // Set prescaler to 64
 
+    Serial.begin(9600);
+    espSerial.begin(9600);
+    Serial.println("Estableciendo conexión...");
     // connectToWIFI();
- Serial.begin(9600);
-  espSerial.begin(9600);
-  Serial.println("Estableciendo conexión...");
-  espSerial.println("AT");
+
+    setupI2C_LCD();
+
   while (1) {
 
-     // Envío de datos de Serial a espSerial
+    uint16_t potValue = ADC_Read(ADC_CHANNEL);
+
+    selectPomodoro(potValue);
+
+
   if (Serial.available()) {
     char c = Serial.read();
-    espSerial.write(c); // Escribe en el ESP8266
+    espSerial.write(c); 
   }
 
-  // Envío de datos de espSerial a Serial
   if (espSerial.available()) {
     char c = espSerial.read();
-    Serial.write(c); // Escribe en el monitor serial
+    Serial.write(c); 
   }
 
-  // Imprimir la respuesta del ESP8266
   printESPResponse(); 
     
     if (ButtonPressed(0, PINB, 0, 100)) {
@@ -119,11 +141,13 @@ int main(void) {
     if(ButtonPressed(1, PINB, 1, 100)) 
     {
       selectCategory();
+      espSerial.println("AT");
     }
     if(ButtonPressed(2, PINB, 2, 100))
     {
       PORTD &= (1 << 1);
-      lcd.clear();
+      clearRow(0);
+      lcd.setCursor(0, 0);
       lcd.print("Timer reseteado");
       resetTimer();
     }
@@ -137,6 +161,12 @@ void basicTimer()
 {
    if(segundos > 0) 
    {
+    if (timerIsRunning)
+    {
+      if (!(PORTD & (1 << PORTD2))) {
+          PORTD |= (1 << PORTD2); 
+        }
+    }
     if (TCNT1 > 63000 && timerIsRunning)
       {
           PORTD &= (1<<2); // Turn off buzzer
@@ -154,47 +184,66 @@ void basicTimer()
    }
    else if( segundos == 0 )
    {
+    if (PORTD & (1 << PORTD2)) {
+    PORTD &= ~(1 << PORTD2); 
+    }
     breakTimer();
    }
 }
 
 void breakTimer()
 {
-  // TODO: Implementar break timer LCD
+
+  PORTD &= (1<<2);
+  if (!(PORTD & (1 << PORTD3))) {
+    PORTD |= (1 << PORTD3); 
+  }
   if(breakSeconds > 0)
   {
-    // TODO: Leds Apagar 2 y prender 3
     if (TCNT1 > 63000 && timerIsRunning)
       {
           TCNT1 = 0;
           breakSeconds--;
-          lcd.clear();
-          lcd.print(breakSeconds);
+          clearRow(0);
+          lcd.setCursor(0, 0);
+          lcd.print("Descanso -> ");
+          lcd.print(segundos);
+          lcd.print(":");
+          lcd.print(count--);
+          if(count == 0) {count = 59;}
       }
   }
   else 
   {
-    resetTimer();
+     if (PORTD & (1 << PORTD3)) {
+    PORTD &= ~(1 << PORTD3); 
+    }
+    clearRow(0);
+    pomos = pomos + 1;  
+    continueTimer();
   }
 }
 
 void resetTimer()
 {
-  breakTimerIsRunning = false; 
   timerIsRunning = false;
-  segundos = 1500;
-  count = 59;
+  segundos = selectSeconds;
+  breakSeconds = selectBreakSeconds; 
+}
+
+void continueTimer()
+{
+  segundos = selectSeconds;
+  breakSeconds = selectBreakSeconds;
+  timerIsRunning = true; 
 }
 
 void connectToWIFI() {
-  Serial.begin(9600);
-  espSerial.begin(9600);
-
   espSerial.println("AT");
-  espSerial.println("AT+CWLAP");
+  _delay_ms(500);
   String command =  "AT+CWJAP=\"" + String(WIFI_SSID) + "\",\"" + String(WIFI_PWD) + "\"";
   espSerial.println(command);
-  
+  _delay_ms(1000);
 }
 
 void setupI2C_LCD() {
@@ -221,24 +270,60 @@ void selectCategory() {
     case 2: 
     selectedCategory = 0; 
     lcd.print("Test 3");
+    lcd.print(pomos);
     break;
     default:
       break;
     }
 }
 
-
-
-
 void printCat() 
 {
   lcd.setCursor(0, 1);
   lcd.print("Cat: ");
   lcd.print(selectedCategory);
+  lcd.print((int)pomos);
 }
 
 void clearRow(byte row)
 {
   lcd.setCursor(0, row);
-  lcd.print("        ");
+  lcd.print("                ");
+}
+
+void selectPomodoro(uint16_t pV)
+{
+ if(!timerIsRunning) 
+ {
+  if(pV == 7)
+    {
+      selectSeconds = 5; 
+      segundos = selectSeconds;
+      selectBreakSeconds = 5; 
+      breakSeconds = selectBreakSeconds;
+      clearRow(0);
+  lcd.setCursor(0, 0);
+      lcd.print("Pomodoro 25/5");
+    } 
+    else if(pV == 605)
+    {
+      selectSeconds = 6; 
+      segundos = selectSeconds;
+      selectBreakSeconds = 6;
+      breakSeconds = selectBreakSeconds;
+      clearRow(0);
+  lcd.setCursor(0, 0);
+      lcd.print("Pomodoro 35/10");
+    } 
+    else if(pV == 1001)
+    {
+      selectSeconds = 7; 
+      segundos = selectSeconds;
+      selectBreakSeconds = 7; 
+      breakSeconds = selectBreakSeconds;
+      clearRow(0);
+      lcd.setCursor(0, 0);
+      lcd.print("Pomodoro 50/10");
+    }
+ }
 }
